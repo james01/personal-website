47a6d9e93bf36dee3242e3cb9b568f4f7cfb8ca5
James Randolph <jwrand16@gmail.com>
2021-04-07 00:22:59 -0400

    Update content, remove bookshelf page

--- src/data/credentials.js
+++ src/data/credentials.js
@@ -1,3 +1,3 @@
 module.exports = {
-  pdfClientId: process.env.ELEVENTY_ENV === "production" ? "87afd7b0ef894e12adcd200c26400a15" : "c4992a64ef5f497a99af313ed3b3d3d8",
+  pdfClientId: "559192ee230045b4b2814d345d4174db",
 };
